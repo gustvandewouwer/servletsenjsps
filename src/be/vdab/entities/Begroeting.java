62f32a4ba9c79eb8936f8dbbcf4a9683e65f5d71
Gust Van de Wouwer <gust.vandewouwer@gmail.com>
2017-05-17 12:09:32 +0200

    commit Java Beans

--- src/be/vdab/entities/Begroeting.java
+++ src/be/vdab/entities/Begroeting.java
@@ -8,4 +8,5 @@ public class Begroeting {
 		int uur = LocalDateTime.now().getHour();
 		return uur >= 6 && uur < 12 ? "Goede morgen" : uur >= 12 && uur < 18 ? "Goede middag" : "Goede avond";
 	}
+
 }
