e9717c3270475c3947ef246bb7ad67a9cfbfd9d6
Gust Van de Wouwer <gust.vandewouwer@gmail.com>
2017-05-22 09:52:19 +0200

    commit: POST request verwerken

--- WebContent/WEB-INF/JSP/pizzas.jsp
+++ WebContent/WEB-INF/JSP/pizzas.jsp
@@ -4,7 +4,7 @@
 <html lang='nl'>
 <head>
 <c:import url='/WEB-INF/JSP/head.jsp'>
-	<c:param name='title' value="Pizza's" />
+	<c:param name='title' value="Pizza's xxx" />
 </c:import>
 </head>
 <body>
@@ -17,15 +17,19 @@
 	</h1>
 	<ul class='zebra'>
 		<c:forEach var='pizza' items='${pizzas}'>
-			<li>
-				${pizza.id}:
-				<c:out value='${pizza.naam}' /> 
-				${pizza.prijs}&euro; 
-				${pizza.pikant ? "pikant" : "niet pikant"} 
-				<c:url value='/pizzas/detail.htm' var='detailURL'>
+			<li>${pizza.id}: 
+				<c:out value='${pizza.naam}' />
+				${pizza.prijs}&euro; ${pizza.pikant ? "pikant" : "niet pikant"} 
+				<c:url
+					value='/pizzas/detail.htm' var='detailURL'>
 					<c:param name='id' value="${pizza.id}" />
 				</c:url> 
-				<a href="<c:out value='${detailURL}'/>">Detail</a>
+				<a href="<c:out value='${detailURL}'/>">Detail</a> 
+				<c:if
+					test='${pizzaIdsMetFoto.contains(pizza.id)}'>
+					<c:url value='/pizzafotos/${pizza.id}.jpg' var='fotoURL' />
+					<a href='${fotoURL}'>Foto</a>
+				</c:if>
 			</li>
 		</c:forEach>
 	</ul>
--- src/be/vdab/servlets/PizzaToevoegenServlet.java
+++ src/be/vdab/servlets/PizzaToevoegenServlet.java
@@ -6,10 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import be.vdab.entities.Pizza;
 import be.vdab.repositories.PizzaRepository;
@@ -19,10 +21,11 @@ import be.vdab.util.StringUtils;
  * Servlet implementation class PizzaToevoegenServlet
  */
 @WebServlet("/pizzas/toevoegen.htm")
+@MultipartConfig
 public class PizzaToevoegenServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/pizzatoevoegen.jsp";
-//	private static final String SUCCESS_VIEW = "/WEB-INF/JSP/pizzas.jsp";
+	// private static final String SUCCESS_VIEW = "/WEB-INF/JSP/pizzas.jsp";
 	private static final String REDIRECT_URL = "%s/pizzas.htm";
 	private final PizzaRepository pizzaRepository = new PizzaRepository();
 
@@ -52,18 +55,30 @@ public class PizzaToevoegenServlet extends HttpServlet {
 		if (StringUtils.isBigDecimal(prijsString)) {
 			prijs = new BigDecimal(prijsString);
 			if (!Pizza.isPrijsValid(prijs)) {
-				
+
 				fouten.put("prijs", "tik een positief getal");
 			}
 		} else {
 			fouten.put("prijs", "tik een getal");
 		}
+
+		Part fotoPart = request.getPart("foto");
+		boolean fotoIsOpgeladen = fotoPart != null && fotoPart.getSize() != 0;
+		if (fotoIsOpgeladen && !fotoPart.getContentType().contains("jpeg")) {
+			fouten.put("foto", "geen JPEG foto");
+		}
 		if (fouten.isEmpty()) {
 			boolean pikant = "pikant".equals(request.getParameter("pikant"));
-			pizzaRepository.create(new Pizza(naam, prijs, pikant));
-//			request.setAttribute("pizzas", pizzaRepository.findAll());
-//			request.getRequestDispatcher(SUCCESS_VIEW).forward(request, response);
-			response.sendRedirect(String.format(REDIRECT_URL, request.getContextPath()));
+			Pizza pizza = new Pizza(naam, prijs, pikant);
+			pizzaRepository.create(pizza);
+			if (fotoIsOpgeladen) {
+				String pizzaFotosPad = this.getServletContext().getRealPath("/pizzafotos");
+				System.out.println("pizzaFotosPad= " + pizzaFotosPad);
+				fotoPart.write(String.format("%s/%d.jpg", pizzaFotosPad, pizza.getId()));
+			}
+			String url = String.format(REDIRECT_URL, request.getContextPath());
+			System.out.println("redirecturl= " + url);
+			response.sendRedirect(url);
 		} else {
 			request.setAttribute("fouten", fouten);
 			request.getRequestDispatcher(VIEW).forward(request, response);
